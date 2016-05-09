ab9369a9fad164e0ef882d1c24b376bc72e46eee
Abhilash <abhilashmyworld@gmail.com>
2016-05-09 18:47:10 +0530

    Fixed viewPager issue
    Commented out behaviour of viewpager in ArticleDetailActivity
    
    I'll work on this later

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
@@ -8,6 +8,7 @@ import android.database.Cursor;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
@@ -55,7 +56,7 @@ public class ArticleDetailActivity extends AppCompatActivity
         mPagerAdapter = new MyPagerAdapter(getFragmentManager());
         mPager = (ViewPager) findViewById(R.id.pager);
         mPager.setAdapter(mPagerAdapter);
-        mPager.setPageMargin((int) TypedValue
+/*        mPager.setPageMargin((int) TypedValue
                 .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, getResources().getDisplayMetrics()));
         mPager.setPageMarginDrawable(new ColorDrawable(0x22000000));
 
@@ -74,9 +75,9 @@ public class ArticleDetailActivity extends AppCompatActivity
                     mCursor.moveToPosition(position);
                 }
                 mSelectedItemId = mCursor.getLong(ArticleLoader.Query._ID);
-              //  updateUpButtonPosition();
+                //  updateUpButtonPosition();
             }
-        });
+        });*/
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
@@ -156,12 +157,13 @@ public class ArticleDetailActivity extends AppCompatActivity
         mUpButton.setTranslationY(Math.min(mSelectedItemUpButtonFloor - upButtonNormalBottom, 0));
     }*/
 
-    private class MyPagerAdapter extends FragmentStatePagerAdapter {
+  //  private class MyPagerAdapter extends FragmentStatePagerAdapter {
+  private class MyPagerAdapter extends FragmentPagerAdapter {
         public MyPagerAdapter(FragmentManager fm) {
             super(fm);
         }
 
-        @Override
+      /*  @Override
         public void setPrimaryItem(ViewGroup container, int position, Object object) {
             super.setPrimaryItem(container, position, object);
             ArticleDetailFragment fragment = (ArticleDetailFragment) object;
@@ -169,7 +171,7 @@ public class ArticleDetailActivity extends AppCompatActivity
              //   mSelectedItemUpButtonFloor = fragment.getUpButtonFloor();
              //   updateUpButtonPosition();
             }
-        }
+        }*/
 
         @Override
         public Fragment getItem(int position) {
--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -103,16 +103,16 @@ public class ArticleDetailFragment extends Fragment implements
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         mRootView = inflater.inflate(R.layout.detail_demo_frag, container, false);
-       /* mDrawInsetsFrameLayout = (DrawInsetsFrameLayout)
-                mRootView.findViewById(R.id.draw_insets_frame_layout);*/
-     /*   mDrawInsetsFrameLayout.setOnInsetsCallback(new DrawInsetsFrameLayout.OnInsetsCallback() {
+      /*  mDrawInsetsFrameLayout = (DrawInsetsFrameLayout)
+                mRootView.findViewById(R.id.draw_insets_frame_layout);
+        mDrawInsetsFrameLayout.setOnInsetsCallback(new DrawInsetsFrameLayout.OnInsetsCallback() {
             @Override
             public void onInsetsChanged(Rect insets) {
                 mTopInset = insets.top;
             }
-        });
+        });*/
 
-        mScrollView = (ObservableScrollView) mRootView.findViewById(R.id.scrollview);
+       /* mScrollView = (ObservableScrollView) mRootView.findViewById(R.id.scrollview);
         mScrollView.setCallbacks(new ObservableScrollView.Callbacks() {
             @Override
             public void onScrollChanged() {
@@ -123,7 +123,7 @@ public class ArticleDetailFragment extends Fragment implements
             }
         });*/
 
-        mPhotoView = (ImageView) mRootView.findViewById(R.id.photo);
+      mPhotoView = (ImageView) mRootView.findViewById(R.id.photo);
         mPhotoContainerView = mRootView.findViewById(R.id.photo_container);
 
         mStatusBarColorDrawable = new ColorDrawable(0);
@@ -138,12 +138,12 @@ public class ArticleDetailFragment extends Fragment implements
             }
         });
 
-       // bindViews();
+        bindViews();
       //  updateStatusBar();
         return mRootView;
     }
 
-   /* private void updateStatusBar() {
+  /*  private void updateStatusBar() {
         int color = 0;
         if (mPhotoView != null && mTopInset != 0 && mScrollY > 0) {
             float f = progress(mScrollY,
@@ -156,11 +156,11 @@ public class ArticleDetailFragment extends Fragment implements
         }
         mStatusBarColorDrawable.setColor(color);
         mDrawInsetsFrameLayout.setInsetBackground(mStatusBarColorDrawable);
-    }*/
-
-   /* static float progress(float v, float min, float max) {
-        return constrain((v - min) / (max - min), 0, 1);
     }
+
+    static float progress(float v, float min, float max) {
+        return constrain((v - min) / (max - min), 0, 1);
+    }*/
 
     static float constrain(float val, float min, float max) {
         if (val < min) {
@@ -170,7 +170,7 @@ public class ArticleDetailFragment extends Fragment implements
         } else {
             return val;
         }
-    }*/
+    }
 
     private void bindViews() {
         if (mRootView == null) {
