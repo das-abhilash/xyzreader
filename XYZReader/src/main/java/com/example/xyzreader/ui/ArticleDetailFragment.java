7be896ab88bb5fd557e847c18d87af78846b0edb
Abhilash <abhilashmyworld@gmail.com>
2016-05-11 10:22:26 +0530

    Changed The font of DetailActivity Body and Style for API level 21
    Font of the body is changed to Calibiri.
    
     API level 21 style xml resource file has been added.

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -197,7 +197,7 @@ public class ArticleDetailFragment extends Fragment implements
         TextView bylineView = (TextView) mRootView.findViewById(R.id.article_byline);
         bylineView.setMovementMethod(new LinkMovementMethod());
         TextView bodyView = (TextView) mRootView.findViewById(R.id.article_body);
-        bodyView.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "Rosario-Regular.ttf"));
+        bodyView.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "Calibri.ttf"));
 
         if (mCursor != null) {
             mRootView.setAlpha(0);
